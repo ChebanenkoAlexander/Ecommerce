5f1b496566303577bb4c81dbae3020a02aef0040
Chebanenko Alexander <chebanenko56@gmail.com>
2026-04-17 15:09:33 +0500

    add sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,7 @@ function Sidebar({ brands }) {
   return (
     <aside className="sidebar">
         <div className="filter-container">
+            <h2>Filters</h2>
             <div className="filter-section">
                 <h3>Brand</h3>
                 <select className="brand-select">
@@ -16,11 +17,10 @@ function Sidebar({ brands }) {
             </div>
             
             <div className="filter-section">
-                <h3>Price</h3>
+                <h3>Price Range</h3>
                 <div className="price-inputs">
-                <input type="number" placeholder="Min" className="price-input" />
-                <span>-</span>
-                <input type="number" placeholder="Max" className="price-input" />
+                    <input type="number" placeholder="0" className="price-input" />
+                    <input type="number" placeholder="5000" className="price-input" />
                 </div>
             </div>
             
@@ -29,9 +29,10 @@ function Sidebar({ brands }) {
 
       
       <div className="special-deal-banner">
-        <h3>🔥 Special Deal!</h3>
-        <div className="timer">0:59:59</div>
-        <p>Limited time offer!</p>
+        <h3><img src="./icons/clock.svg" alt="" /> Special Deal!</h3>
+        <p className='special-deal-banner__text'>Register now to unlock exclusive offers and discounts</p>
+        <p>Offer expires in: <span className="timer">0:53:50</span></p>
+        <img className='special-deal-banner__close' src="./icons/close.svg" alt="" />
       </div>
     </aside>
   );
